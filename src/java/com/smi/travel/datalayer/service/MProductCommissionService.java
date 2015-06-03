584e6cb4417ee437f7ab6e82d3a36ce17f62d1bb
surachai.ns <surachai.ns@gmail.com>
2015-06-03 15:38:39 +0700

    update service  product Commission

---
+++ src/java/com/smi/travel/datalayer/service/MProductCommissionService.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.smi.travel.datalayer.service;
+
+import com.smi.travel.datalayer.dao.ProductDao;
+import com.smi.travel.datalayer.entity.Product;
+import java.util.List;
+
+/**
+ *
+ * @author Surachai
+ */
+public class MProductCommissionService {
+    private ProductDao productDao;
+    public List<Product> getListMasterProduct(){
+        return productDao.getListProduct();
+    }
+}
