3a14bc143f14b9277aefbef29184712f29c7c12c
surachai.ns <surachai.ns@gmail.com>
2015-06-19 19:09:37 +0700

    add passenger ajax

--- web/js/PassengerDetail.js
+++ web/js/PassengerDetail.js
@@ -190,15 +190,21 @@ $(document).ready(function () {
     // PASSENGER TABLE
     $('#CustomerTable').dataTable({bJQueryUI: true,
         "sPaginationType": "full_numbers",
-        "bAutoWidth": false,
-        "bFilter": true,
-        "bPaginate": true,
-        "bInfo": false,
-        "bLengthChange": false,
-        "iDisplayLength": 10
+                        "bAutoWidth": false,
+                        "bFilter": false,
+                        "bPaginate": true,
+                        "bInfo": false,
+                        "bLengthChange": false,
+                        "iDisplayLength": 10
     });
     $('#CustomerTable tbody').on('click', 'tr', function () {
         $(this).addClass('row_selected').siblings().removeClass('row_selected');
+    });
+    
+     $("#filtercus").keyup(function (event) {
+        if (event.keyCode === 13) {
+            FilterCustomerList($("#filtercus").val());
+        }
     });
 });
 
@@ -206,4 +212,94 @@ function _calculateAge(birthday) {
     var ageDifMs = Date.now() - birthday.getTime();
     var ageDate = new Date(ageDifMs);
     return Math.abs(ageDate.getUTCFullYear() - 1970);
+}
+
+function FilterCustomerList(name) {
+    // var name = 
+    var servletName = 'PassengerServlet';
+    var servicesName = 'AJAXBean';
+    var param = 'action=' + 'text' +
+            '&servletName=' + servletName +
+            '&servicesName=' + servicesName +
+            '&name=' + name +
+            '&type=' + 'searchPassenger';
+    CallFilterAjax(param);
+    
+}
+
+function CallFilterAjax(param) {
+    var url = 'AJAXServlet';
+    $("#ajaxload").removeClass("hidden");
+    try {
+        $.ajax({
+            type: "POST",
+            url: url,
+            cache: false,
+            data: param,
+            success: function (msg) {
+                try {
+                   
+                    $('#CustomerTable').dataTable().fnClearTable();
+                    $('#CustomerTable').dataTable().fnDestroy();
+                    $("#CustomerTable tbody").empty().append(msg);
+                    $("#CustomerTable tr").on('click', function () {
+                    var customer_id = $(this).find(".customer-id").text();
+                    var customer_code = $(this).find(".customer-code").text();
+                    var customer_initial = $(this).find(".customer-initial").text();
+                    var customer_initialId = $(this).find(".customer-initialId").text();
+                    var customer_firstname = $(this).find(".customer-firstname").text();
+        var customer_lastname = $(this).find(".customer-lastname").text();
+        var customer_sex = $(this).find(".customer-sex").text();
+        var customer_address = $(this).find(".customer-address").text();
+        var customer_tel = $(this).find(".customer-tel").text();
+        var customer_phone = $(this).find(".customer-phone").text();
+        var customer_postal = $(this).find(".customer-postal").text();
+        var customer_email = $(this).find(".customer-email").text();
+        var customer_remark = $(this).find(".customer-remark").text();
+        var customer_passportno = $(this).find(".customer-passportno").text();
+        var customer_japanfirstname = $(this).find(".customer-japanfirstname").text();
+        var customer_japanlastname = $(this).find(".customer-japanlastname").text();
+        $("#customerId").val(customer_id);
+        $("#MInitialname").val(customer_initialId);
+        $("#passengerId").val(customer_code);
+        $("#firstName").val(customer_firstname);
+        $("#lastName").val(customer_lastname);
+        $("#sex").val(customer_sex);
+        $("#address").val(customer_address);
+        $("#tel").val(customer_tel);
+        $("#phone").val(customer_phone);
+        $("#email").val(customer_email);
+        $("#remark").val(customer_remark);
+        $("#Passport").val(customer_passportno);
+        $("#CustomerModal").modal('hide');
+    });
+                    $('#CustomerTable').dataTable({bJQueryUI: true,
+                        "sPaginationType": "full_numbers",
+                        "bAutoWidth": false,
+                        "bFilter": false,
+                        "bPaginate": true,
+                        "bInfo": false,
+                        "bLengthChange": false,
+                        "iDisplayLength": 10
+                    });
+                     $("#ajaxload").addClass("hidden");
+                     
+                
+                
+                
+ 
+                } catch (e) {
+                    alert(e);
+                }
+
+            }, error: function (msg) {
+                 $("#ajaxload").addClass("hidden");
+                alert(msg);
+            }
+        });
+    } catch (e) {
+        alert(e);
+    }
+
+
 }
