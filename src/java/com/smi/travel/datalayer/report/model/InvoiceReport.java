540fb8f420c8ac31556f15b76e9a6a899ffd955a
weewii <wee.chonnasith@gmail.com>
2015-07-06 10:38:32 +0700

    test Commit by wee

---
+++ src/java/com/smi/travel/datalayer/report/model/InvoiceReport.java
@@ -0,0 +1,186 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.smi.travel.datalayer.report.model;
+
+/**
+ *
+ * @author chonnasith
+ */
+public class InvoiceReport {
+    private String invto;
+    private String invno;
+    private String invdate;
+    private String refno;
+    private String staff;
+    private String payment;
+    private String description;
+    private String gross;
+    private String vat;
+    private String amount;
+    private String total;
+    private int grtotal;
+    private String accname;
+    private String bank;
+    private String branch;
+    private String accno;
+    private String acctype;
+    private String user;
+    private String textmoney;
+
+    public String getInvto() {
+        return invto;
+    }
+
+    public void setInvto(String invto) {
+        this.invto = invto;
+    }
+
+    public String getInvno() {
+        return invno;
+    }
+
+    public void setInvno(String invno) {
+        this.invno = invno;
+    }
+
+    public String getInvdate() {
+        return invdate;
+    }
+
+    public void setInvdate(String invdate) {
+        this.invdate = invdate;
+    }
+
+    public String getRefno() {
+        return refno;
+    }
+
+    public void setRefno(String refno) {
+        this.refno = refno;
+    }
+
+    public String getStaff() {
+        return staff;
+    }
+
+    public void setStaff(String staff) {
+        this.staff = staff;
+    }
+
+    public String getPayment() {
+        return payment;
+    }
+
+    public void setPayment(String payment) {
+        this.payment = payment;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getGross() {
+        return gross;
+    }
+
+    public void setGross(String gross) {
+        this.gross = gross;
+    }
+
+    public String getVat() {
+        return vat;
+    }
+
+    public void setVat(String vat) {
+        this.vat = vat;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
+    public String getTotal() {
+        return total;
+    }
+
+    public void setTotal(String total) {
+        this.total = total;
+    }
+
+    public int getGrtotal() {
+        return grtotal;
+    }
+
+    public void setGrtotal(int grtotal) {
+        this.grtotal = grtotal;
+    }
+
+    public String getAccname() {
+        return accname;
+    }
+
+    public void setAccname(String accname) {
+        this.accname = accname;
+    }
+
+    public String getBank() {
+        return bank;
+    }
+
+    public void setBank(String bank) {
+        this.bank = bank;
+    }
+
+    public String getBranch() {
+        return branch;
+    }
+
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
+
+    public String getAccno() {
+        return accno;
+    }
+
+    public void setAccno(String accno) {
+        this.accno = accno;
+    }
+
+    public String getAcctype() {
+        return acctype;
+    }
+
+    public void setAcctype(String acctype) {
+        this.acctype = acctype;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getTextmoney() {
+        return textmoney;
+    }
+
+    public void setTextmoney(String textmoney) {
+        this.textmoney = textmoney;
+    }    
+       
+}
