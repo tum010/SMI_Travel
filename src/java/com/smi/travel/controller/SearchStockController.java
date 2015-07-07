0c89e0a8d09feab297b7d768ad54db419bc13830
surachai.ns <surachai.ns@gmail.com>
2015-07-08 01:52:41 +0700

    add structure  stock

--- src/java/com/smi/travel/controller/SearchStockController.java
+++ src/java/com/smi/travel/controller/SearchStockController.java
@@ -1,4 +1,5 @@
 package com.smi.travel.controller;
+import com.smi.travel.datalayer.service.StockService;
 import com.smi.travel.master.controller.SMITravelController;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -6,6 +7,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 public class SearchStockController extends SMITravelController {
+    private StockService   stockService;
     private static final ModelAndView SearchStock = new ModelAndView("SearchStock");
     private static final ModelAndView SearchStock_REFRESH = new ModelAndView(new RedirectView("SearchStock.smi", true));
     
@@ -13,4 +15,14 @@ public class SearchStockController extends SMITravelController {
     protected ModelAndView process(HttpServletRequest request, HttpServletResponse response, HttpSession session) {
         return SearchStock;
     }
+
+    public StockService getStockService() {
+        return stockService;
+    }
+
+    public void setStockService(StockService stockService) {
+        this.stockService = stockService;
+    }
+    
+    
 }
