b1e965dc485d6683967b25ad1fd757ad3ffa9357
weewii <wee.chonnasith@gmail.com>
2015-07-08 13:55:03 +0700

    invoice

---
+++ src/java/com/smi/travel/datalayer/view/dao/ReceiptDao.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.smi.travel.datalayer.view.dao;
+
+import com.smi.travel.datalayer.report.model.ReceiptEmail;
+
+/**
+ *
+ * @author chonnasith
+ */
+public interface ReceiptDao {
+    public ReceiptEmail getReceipt();   
+}
