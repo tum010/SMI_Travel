a839e3f2a7d9e14316b2f6a6c5a142eedd22f969
weewii <wee.chonnasith@gmail.com>
2015-07-08 14:11:34 +0700

    invoice

--- src/java/com/smi/travel/datalayer/view/dao/impl/InvoiceEmailImpl.java
+++ src/java/com/smi/travel/datalayer/view/dao/impl/InvoiceEmailImpl.java
@@ -6,7 +6,7 @@
 
 package com.smi.travel.datalayer.view.dao.impl;
 import com.smi.travel.datalayer.report.model.InvoiceReport;
-import com.smi.travel.datalayer.view.dao.InvoiceReportDao;
+import com.smi.travel.datalayer.view.dao.InvoiceEmailDao;
 import com.smi.travel.util.UtilityFunction;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -19,38 +19,38 @@ import org.hibernate.SessionFactory;
  *
  * @author chonnasith
  */
-public class InvoiceReportImpl implements InvoiceReportDao{
+public class InvoiceEmailImpl implements InvoiceEmailDao{
     private SessionFactory sessionFactory;
-     private UtilityFunction utilityFunction;
+    private UtilityFunction utilityFunction;
 
     @Override
-    public InvoiceReport getInvoiceReport() {
+    public InvoiceReport getInvoiceEmail() {
         Session session = this.sessionFactory.openSession();
         UtilityFunction util = new UtilityFunction();  
-        InvoiceReport invoiceReport = new InvoiceReport();
-        invoiceReport.setAccname("TestMan");
-        invoiceReport.setAccno("11111111");
-        invoiceReport.setAcctype("Test");
-        invoiceReport.setAmount("1000000000");
-        invoiceReport.setBank("KBank");
-        invoiceReport.setBranch("Bangkapi");
-        invoiceReport.setDescription("test ภาษาไทย");
-        invoiceReport.setGross("999999");
+        InvoiceReport invoiceEmail = new InvoiceReport();
+        invoiceEmail.setAccname("TestMan");
+        invoiceEmail.setAccno("11111111");
+        invoiceEmail.setAcctype("Test");
+        invoiceEmail.setAmount("1000000000");
+        invoiceEmail.setBank("KBank");
+        invoiceEmail.setBranch("Bangkapi");
+        invoiceEmail.setDescription("test ภาษาไทย");
+        invoiceEmail.setGross("999999");
         int a = 1999999999;
-        invoiceReport.setGrtotal("1999999999");
-        invoiceReport.setInvdate("01-12-15");
-        invoiceReport.setInvno("123456789");
-        invoiceReport.setInvto("Iconext Thailand co.ltd");
-        invoiceReport.setPayment("Mr. Test Man");
-        invoiceReport.setRefno("11111111");
-        invoiceReport.setStaff("Minions");
-        invoiceReport.setTotal("9999999999");
-        invoiceReport.setUser("Mike Jr. Robert");
-        invoiceReport.setVat("79999");
-        invoiceReport.setTextmoney(utilityFunction.convert(a)+" baht");
+        invoiceEmail.setGrtotal("1999999999");
+        invoiceEmail.setInvdate("01-12-15");
+        invoiceEmail.setInvno("123456789");
+        invoiceEmail.setInvto("Iconext Thailand co.ltd");
+        invoiceEmail.setPayment("Mr. Test Man");
+        invoiceEmail.setRefno("11111111");
+        invoiceEmail.setStaff("Minions");
+        invoiceEmail.setTotal("9999999999");
+        invoiceEmail.setUser("Mike Jr. Robert");
+        invoiceEmail.setVat("79999");
+        invoiceEmail.setTextmoney(utilityFunction.convert(a)+" baht");
         session.close();
         this.sessionFactory.close();
-        return invoiceReport;
+        return invoiceEmail;
     }
 
     public SessionFactory getSessionFactory() {
