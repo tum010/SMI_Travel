abb6759e443e4f64e009371a3ca28a1791e8cd05
weewii <wee.chonnasith@gmail.com>
2015-07-09 16:29:00 +0700

    Payment Tour Hotel

---
+++ src/java/com/smi/travel/datalayer/view/dao/InvoiceSuppilerDao.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.smi.travel.datalayer.view.dao;
+
+import com.smi.travel.datalayer.view.entity.InvoiceSupplier;
+import java.util.List;
+/**
+ *
+ * @author chonnasith
+ */
+public interface InvoiceSuppilerDao {
+    public List<InvoiceSupplier> getListInvoiceSupplier();   
+}
