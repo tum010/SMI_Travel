fb20cfae407a27bfe74af9f49e0193e5662c0ff0
weewii <wee.chonnasith@gmail.com>
2015-07-10 15:56:47 +0700

    Air Ticket Detail

--- src/java/com/smi/travel/datalayer/view/dao/impl/InvoiceSuppilerImpl.java
+++ src/java/com/smi/travel/datalayer/view/dao/impl/InvoiceSuppilerImpl.java
@@ -7,6 +7,7 @@
 package com.smi.travel.datalayer.view.dao.impl;
 import com.smi.travel.datalayer.view.dao.InvoiceSuppilerDao;
 import com.smi.travel.datalayer.view.entity.InvoiceSupplier;
+import com.smi.travel.util.UtilityFunction;
 import java.util.List;
 import org.hibernate.Hibernate;
 import org.hibernate.Session;
@@ -31,12 +32,29 @@ public class InvoiceSuppilerImpl implements InvoiceSuppilerDao{
     @Override
     public List<InvoiceSupplier> getListInvoiceSupplier() {
         Session session = this.sessionFactory.openSession();
+        UtilityFunction util = new UtilityFunction();
         List<InvoiceSupplier> invoiceSupplierList = session.createSQLQuery(" SELECT * FROM `invoice_supplier` ")
                 .addScalar("id", Hibernate.STRING)
                 .addScalar("code", Hibernate.STRING)
                 .addScalar("name", Hibernate.STRING)
                 .addScalar("apcode", Hibernate.STRING)
-                .list();        
+                .list();
+        
+//        for (List<InvoiceSupplier> A : invoiceSupplierList) {
+//            InvoiceSupplier invoiceSupplier = new InvoiceSupplier();
+//            invoiceSupplier.setId(String.valueOf(A));
+//            invoiceSupplier.setCode(String.valueOf(A));
+//        }
+        
+//        for (int i=0;i<=invoiceSupplierList.size();i++) {
+//            InvoiceSupplier invoiceSupplierListRe = new InvoiceSupplier();
+//            System.out.println("invoiceSupplierList.get(i).getId()" +invoiceSupplierList.get(i).getId());
+//            invoiceSupplierListRe.setId(util.ConvertString(invoiceSupplierList.get(i).getId()));
+//            invoiceSupplierListRe.setCode(util.ConvertString(invoiceSupplierList.get(i).getCode()));
+//            invoiceSupplierListRe.setName(util.ConvertString(invoiceSupplierList.get(i).getName()));
+//            invoiceSupplierListRe.setApcode(util.ConvertString(invoiceSupplierList.get(i).getApcode()));
+//
+//         }
         return invoiceSupplierList;
     }
     
