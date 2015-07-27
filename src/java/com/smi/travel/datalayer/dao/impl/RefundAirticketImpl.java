052f79b5bceeb344220e2035b8ca6867ecec5e46
surachai.ns <surachai.ns@gmail.com>
2015-07-27 09:58:38 +0700

    update structure of RefundAirline

---
+++ src/java/com/smi/travel/datalayer/dao/impl/RefundAirticketImpl.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.smi.travel.datalayer.dao.impl;
+
+import com.smi.travel.datalayer.dao.RefundAirticketDao;
+import com.smi.travel.datalayer.entity.RefundAirticket;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+/**
+ *
+ * @author Surachai
+ */
+public class RefundAirticketImpl implements RefundAirticketDao{
+    private SessionFactory sessionFactory;
+    private Transaction transaction;
+
+    @Override
+    public String InsertRefundAirticket(RefundAirticket refund) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String UpdateRefundAirticket(RefundAirticket refund) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String DeleteRefundAirticket(RefundAirticket refund) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String DeleteRefundAirticketDetail(String refundDetailID) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public RefundAirticket getRefundAirTicketFromRefundNo(String RefundNo) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+    
+    
+    
+    
+}
