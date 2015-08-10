92a5116940e35af4fad058aa34a580a577150561
surachai.ns <surachai.ns@gmail.com>
2015-08-10 07:15:36 +0700

    add receipt dao

--- src/java/com/smi/travel/datalayer/dao/ReceiptDao.java
+++ src/java/com/smi/travel/datalayer/dao/ReceiptDao.java
@@ -6,10 +6,17 @@
 
 package com.smi.travel.datalayer.dao;
 
+import com.smi.travel.datalayer.entity.Receipt;
+import java.util.HashMap;
+import java.util.List;
+
 /**
  *
  * @author Surachai
  */
 public interface ReceiptDao {
-     
+    public List<HashMap<String,Object>> getInvoiceFromInvoiceNumber(String InvoiceNumber);
+    public Receipt getReceiptfromReceiptNo(String ReceiptNo);
+    public String UpdateFinanceStatusReceipt(String ReceiptId,int status);
+    // from Receipt rec where rec.MFinanceItemstatus.id = :status and rec.id = :ReceiptId
 }
