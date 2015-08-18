c0947ee3a740f394d134c2c0353def00ea7061d0
nanthachaip <nanthachai@iconext.co.th>
2015-08-18 09:34:05 +0700

    Update refundAirline.js

--- web/js/refundAirline.js
+++ web/js/refundAirline.js
@@ -2,7 +2,6 @@ var deleteRowNo = 0;
 $(document).ready(function () {
     $(".datetime").datetimepicker({
     });
-
 //    $(".datemask").mask('0000-00-00', {reverse: true});
     addRowRefundAirlineList();
     $(".decimal").inputmask({
