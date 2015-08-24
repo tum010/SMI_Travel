b590b524a0424bfa5bb207a831658ed05d4ea75c
weewii <wee.chonnasith@gmail.com>
2015-08-24 23:05:30 +0700

    fix bug send email.

--- src/java/com/smi/travel/controller/mail/SendEmailController.java
+++ src/java/com/smi/travel/controller/mail/SendEmailController.java
@@ -36,11 +36,8 @@ public class SendEmailController extends SMITravelController {
     private static final Logger LOG = Logger.getLogger(SendEmailController.class.getName());
     private static final String InvoiceReport = "Invoice";
     private static final String ReceiptEmail = "ReceiptEmail";
-<<<<<<< HEAD
     private static final String TaxInvoiceEmail = "TaxInvoiceEmail";
-=======
     private static final String CreditNote = "CreditNote";
->>>>>>> c75eb74fda5f0fed746ec24ec28a6f20ce3b26b2
     private ModelAndView ModelMail = new ModelAndView("SendMail");
     private static final String ReportName = "reportname";
     private static final String ReportID = "reportid";
@@ -107,6 +104,11 @@ public class SendEmailController extends SMITravelController {
                 JRDataSource dataSource = new JRBeanCollectionDataSource(data);
                 jasperFileName = "TaxInvoiceEmailReport.jasper";
                 pdfFileName = "taxinvoice.pdf";
+                pathAttachfile = path[0] + "\\" + username;
+                System.out.println("path : " + path[0] + username);
+                if (checkDirectory(path[0] + username)) {
+                    result = reportservice.printreport(jasperFileName, username + "\\" + pdfFileName, dataSource);
+                }
             }
             if (CreditNote.equalsIgnoreCase(name)) {
                 data = reportservice.getCreditNoteReport(reportid);
@@ -118,7 +120,8 @@ public class SendEmailController extends SMITravelController {
                 if (checkDirectory(path[0] + username)) {
                     result = reportservice.printreport(jasperFileName, username + "\\" + pdfFileName, dataSource);
                 }
-            }                      
+            }
+        }    
         System.out.println("result : " + result);
         // sends the e-mail
         if ("success".equalsIgnoreCase(result)) {
@@ -152,6 +155,7 @@ public class SendEmailController extends SMITravelController {
         request.setAttribute(SHOWLEADER, showleader);
         return ModelMail;
     }
+        
 
     public boolean checkDirectory(String pathFile) {
         File f = new File(pathFile);
