b424f275ab65ab7ebc12cdc655db642133e5100e
somchayng <kitty.somchayng@gmail.com>
2015-09-14 09:17:00 +0700

    excel

--- src/java/com/smi/travel/controller/excel/ExportDataToExcelView.java
+++ src/java/com/smi/travel/controller/excel/ExportDataToExcelView.java
@@ -468,6 +468,194 @@ public class ExportDataToExcelView extends AbstractExcelView {
         }
     }
     
+    private void getBillAirAgentReport(HSSFWorkbook wb, List TicketAgent) {
+        String sheetName = "Sheet1";// name of sheet
+        HSSFSheet sheet = wb.createSheet(sheetName);
+        
+        TicketFareReport dataheader = new TicketFareReport();
+        
+        if(TicketAgent != null){
+            dataheader = (TicketFareReport)TicketAgent.get(0);
+        }
+
+        // set Header Report (Row 1)
+        HSSFCellStyle styleC1 = wb.createCellStyle();
+        HSSFRow row1 = sheet.createRow(0);
+        HSSFCell cell1 = row1.createCell(0);
+        cell1.setCellValue("Bill Agent Air");
+        styleC1.setFont(getHeaderFont(wb.createFont()));
+        cell1.setCellStyle(styleC1);
+        sheet.addMergedRegion(CellRangeAddress.valueOf("A1:F1"));
+        
+        // Set align Text
+        HSSFCellStyle styleC21 = wb.createCellStyle();
+        styleC21.setAlignment(styleC21.ALIGN_RIGHT);
+        HSSFCellStyle styleC22 = wb.createCellStyle();
+        styleC22.setAlignment(styleC22.ALIGN_LEFT);
+
+        // Row 2
+        HSSFRow row2 = sheet.createRow(1);
+        HSSFCell cell21 = row2.createCell(0);
+        cell21.setCellValue("Print By : ");
+        cell21.setCellStyle(styleC21);
+        HSSFCell cell22 = row2.createCell(1);
+        cell22.setCellValue(dataheader.getPrintby());
+        cell22.setCellStyle(styleC22);
+        sheet.addMergedRegion(CellRangeAddress.valueOf("B2:D2"));
+        HSSFCell cell23 = row2.createCell(4);
+        cell23.setCellValue("Air Line : ");
+        cell23.setCellStyle(styleC21);
+        HSSFCell cell24 = row2.createCell(5);
+        cell24.setCellValue(dataheader.getAirline());
+        cell24.setCellStyle(styleC22);
+        
+        // Row 3
+        HSSFRow row3 = sheet.createRow(2);
+        HSSFCell cell31 = row3.createCell(0);
+        cell31.setCellValue("Department : ");
+        cell31.setCellStyle(styleC21);
+        HSSFCell cell32 = row3.createCell(1);
+        cell32.setCellValue(dataheader.getHeaddepartment());
+        cell32.setCellStyle(styleC22);
+        sheet.addMergedRegion(CellRangeAddress.valueOf("B3:D3"));
+        HSSFCell cell33 = row3.createCell(4);
+        cell33.setCellValue("Ticket Type : ");
+        cell33.setCellStyle(styleC21);
+        HSSFCell cell34 = row3.createCell(5);
+        cell34.setCellValue(dataheader.getTickettype());
+        cell34.setCellStyle(styleC22);
+
+        // Row 4
+        HSSFRow row4 = sheet.createRow(3);
+        HSSFCell cell41 = row4.createCell(0);
+        cell41.setCellValue("Term Pay : ");
+        cell41.setCellStyle(styleC21);
+        HSSFCell cell42 = row4.createCell(1);
+        cell42.setCellValue(dataheader.getHeadtermpay());
+        cell42.setCellStyle(styleC22);
+        sheet.addMergedRegion(CellRangeAddress.valueOf("B4:D4"));
+        HSSFCell cell43 = row4.createCell(4);
+        cell43.setCellValue("Ticket Buy : ");
+        cell43.setCellStyle(styleC21);
+        HSSFCell cell44 = row4.createCell(5);
+        cell44.setCellValue(dataheader.getTicketbuy());
+        cell44.setCellStyle(styleC22);
+
+        // Row 5
+        HSSFRow row5 = sheet.createRow(4);
+        HSSFCell cell51 = row5.createCell(0);
+        cell51.setCellValue("Sale Staff : ");
+        cell51.setCellStyle(styleC21);
+        sheet.addMergedRegion(CellRangeAddress.valueOf("A5:E5"));
+        HSSFCell cell52 = row5.createCell(5);
+        cell52.setCellValue(dataheader.getHeadstaff());
+        cell52.setCellStyle(styleC22);
+        
+         // Header Table
+        HSSFCellStyle styleC3 = wb.createCellStyle();
+        styleC3.setFont(getHeaderTable(wb.createFont()));
+        HSSFRow row6 = sheet.createRow(8);
+        HSSFCell cell61 = row6.createCell(0);
+        cell61.setCellValue("Invoice No.");
+        cell61.setCellStyle(styleC3);
+        sheet.autoSizeColumn(0);
+        HSSFCell cell62 = row6.createCell(1);
+        cell62.setCellValue("Issue Date");
+        cell62.setCellStyle(styleC3);
+        sheet.autoSizeColumn(1);
+        HSSFCell cell63 = row6.createCell(2);
+        cell63.setCellValue("Agent");
+        sheet.autoSizeColumn(2);
+        cell63.setCellStyle(styleC3);
+        HSSFCell cell64 = row6.createCell(3);
+        cell64.setCellValue("Ticket No.");
+        cell64.setCellStyle(styleC3);
+        sheet.autoSizeColumn(3);
+        HSSFCell cell65 = row6.createCell(4);
+        cell65.setCellValue("Department");
+        cell65.setCellStyle(styleC3);
+        sheet.autoSizeColumn(4);
+        HSSFCell cell66 = row6.createCell(5);
+        cell66.setCellValue("Staff");
+        cell66.setCellStyle(styleC3);
+        sheet.autoSizeColumn(5);
+        HSSFCell cell67 = row6.createCell(6);
+        cell67.setCellValue("Term Pay");
+        cell67.setCellStyle(styleC3);
+        sheet.autoSizeColumn(6);
+        HSSFCell cell68 = row6.createCell(7);
+        cell68.setCellValue("Inv. Amount");
+        cell68.setCellStyle(styleC3);
+        sheet.autoSizeColumn(7);
+        HSSFCell cell69 = row6.createCell(8);
+        cell69.setCellValue("Ticket Comm");
+        cell69.setCellStyle(styleC3);
+        sheet.autoSizeColumn(8);
+        HSSFCell cell70 = row6.createCell(9);
+        cell70.setCellValue("Sale Price");
+        cell70.setCellStyle(styleC3);
+        sheet.autoSizeColumn(9);
+
+        HSSFCell cell71 = row6.createCell(10);
+        cell71.setCellValue("Agent Comm");
+        cell71.setCellStyle(styleC3);
+        sheet.autoSizeColumn(10);
+
+        HSSFCell cell72 = row6.createCell(11);
+        cell72.setCellValue("Profit");
+        cell72.setCellStyle(styleC3);
+        sheet.autoSizeColumn(11);
+
+        //Detail of Table
+        int count = 9 ;
+        for(int i=0;i<TicketAgent.size();i++){
+             TicketFareReport data = (TicketFareReport)TicketAgent.get(i);
+             HSSFRow row = sheet.createRow(count + i);
+             row.createCell(0).setCellValue(data.getInvno());
+             row.createCell(1).setCellValue(data.getIssuedate());
+             row.createCell(2).setCellValue(data.getAgent());
+             row.createCell(3).setCellValue(data.getDocno());
+             row.createCell(4).setCellValue(data.getDepartment());
+             row.createCell(5).setCellValue(data.getStaff());
+             row.createCell(6).setCellValue(data.getTermpay());
+             row.createCell(7).setCellValue(data.getInvamount());
+             row.createCell(8).setCellValue(data.getTicketcom());
+             row.createCell(9).setCellValue(data.getSaleprice());
+             row.createCell(10).setCellValue(data.getAgentcom());
+             row.createCell(11).setCellValue(data.getProfit());
+             if(i == (TicketAgent.size()-1)){
+                row = sheet.createRow(count + i + 1);
+                row.createCell(6).setCellValue("Total");               
+                BigDecimal invAmountTotal = new BigDecimal("0.00");
+                BigDecimal ticketCommTotal = new BigDecimal("0.00");
+                BigDecimal salePriceTotal = new BigDecimal("0.00");
+                BigDecimal agentCommTotal = new BigDecimal("0.00");
+                BigDecimal profitTotal = new BigDecimal("0.00");
+                for(int k=0;k<TicketAgent.size();k++){
+                    TicketFareReport sum = (TicketFareReport)TicketAgent.get(k);
+                    BigDecimal invAmount = new BigDecimal(!"".equalsIgnoreCase(sum.getInvamount()) ? sum.getInvamount() : "0.00");
+                    BigDecimal ticketComm = new BigDecimal(!"".equalsIgnoreCase(sum.getTicketcom()) ? sum.getTicketcom() : "0.00");
+                    BigDecimal salePrice = new BigDecimal(!"".equalsIgnoreCase(sum.getSaleprice()) ? sum.getSaleprice() : "0.00");
+                    BigDecimal agentComm = new BigDecimal(!"".equalsIgnoreCase(sum.getAgentcom()) ? sum.getAgentcom() : "0.00");
+                    BigDecimal profit = new BigDecimal(!"".equalsIgnoreCase(sum.getProfit()) ? sum.getProfit() : "0.00");
+                    invAmountTotal = invAmountTotal.add(invAmount);
+                    ticketCommTotal = ticketCommTotal.add(ticketComm);
+                    salePriceTotal = salePriceTotal.add(salePrice);
+                    agentCommTotal = agentCommTotal.add(agentComm);
+                    profitTotal = profitTotal.add(profit);
+                }  
+                row.createCell(7).setCellValue(String.valueOf(invAmountTotal));
+                row.createCell(8).setCellValue(String.valueOf(ticketCommTotal));
+                row.createCell(9).setCellValue(String.valueOf(salePriceTotal));
+                row.createCell(10).setCellValue(String.valueOf(agentCommTotal));
+                row.createCell(11).setCellValue(String.valueOf(profitTotal));
+             }
+             for(int j =0;j<13;j++){
+                 sheet.autoSizeColumn(j);
+             }
+        }     
+    }
+    
     public void genTicketFareAirlineReport(HSSFWorkbook wb, List TicketFare) {
 
         String sheetName = "Sheet1";// name of sheet
