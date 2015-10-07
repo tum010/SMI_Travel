5801f9d501729cf9d35a3070f8f02646161650ae
somchayng <kitty.somchayng@gmail.com>
2015-10-07 16:46:30 +0700

    RefundTicketDetail

---
+++ src/java/com/smi/travel/datalayer/view/entity/RefundTicketView.java
@@ -0,0 +1,131 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.smi.travel.datalayer.view.entity;
+
+/**
+ *
+ * @author Kanokporn
+ */
+public class RefundTicketView {
+    private String  refundno;					
+    private String  paydate;					
+    private String  ticketno;					
+    private String  refundto;					
+    private String  refundby;					
+    private String  sectorrefund;					
+    private String  receiveairline;					
+    private String  refunddate	;				
+    private String  paycustomer;					
+    private String  paydate2;					
+    private String  profit;					
+    private String  receiveairline2;					
+    private String  airlinecomm	;
+
+    public String getRefundno() {
+        return refundno;
+    }
+
+    public void setRefundno(String refundno) {
+        this.refundno = refundno;
+    }
+
+    public String getPaydate() {
+        return paydate;
+    }
+
+    public void setPaydate(String paydate) {
+        this.paydate = paydate;
+    }
+
+    public String getTicketno() {
+        return ticketno;
+    }
+
+    public void setTicketno(String ticketno) {
+        this.ticketno = ticketno;
+    }
+
+    public String getRefundto() {
+        return refundto;
+    }
+
+    public void setRefundto(String refundto) {
+        this.refundto = refundto;
+    }
+
+    public String getRefundby() {
+        return refundby;
+    }
+
+    public void setRefundby(String refundby) {
+        this.refundby = refundby;
+    }
+
+    public String getSectorrefund() {
+        return sectorrefund;
+    }
+
+    public void setSectorrefund(String sectorrefund) {
+        this.sectorrefund = sectorrefund;
+    }
+
+    public String getReceiveairline() {
+        return receiveairline;
+    }
+
+    public void setReceiveairline(String receiveairline) {
+        this.receiveairline = receiveairline;
+    }
+
+    public String getRefunddate() {
+        return refunddate;
+    }
+
+    public void setRefunddate(String refunddate) {
+        this.refunddate = refunddate;
+    }
+
+    public String getPaycustomer() {
+        return paycustomer;
+    }
+
+    public void setPaycustomer(String paycustomer) {
+        this.paycustomer = paycustomer;
+    }
+
+    public String getPaydate2() {
+        return paydate2;
+    }
+
+    public void setPaydate2(String paydate2) {
+        this.paydate2 = paydate2;
+    }
+
+    public String getProfit() {
+        return profit;
+    }
+
+    public void setProfit(String profit) {
+        this.profit = profit;
+    }
+
+    public String getReceiveairline2() {
+        return receiveairline2;
+    }
+
+    public void setReceiveairline2(String receiveairline2) {
+        this.receiveairline2 = receiveairline2;
+    }
+
+    public String getAirlinecomm() {
+        return airlinecomm;
+    }
+
+    public void setAirlinecomm(String airlinecomm) {
+        this.airlinecomm = airlinecomm;
+    }
+    
+}
