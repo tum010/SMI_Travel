3c36e5d5eeb34e745646aaa0124339d1ce68167e
weewii <wee.chonnasith@gmail.com>
2016-03-17 14:02:01 +0700

    import model nirvana.

---
+++ src/java/com/smi/travel/model/nirvana/SsDataexch.java
@@ -0,0 +1,152 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.smi.travel.model.nirvana;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ *
+ * @author chonnasith
+ */
+public class SsDataexch {
+    private String dataCd;
+    private String dataNo;
+    private String entSysCd;
+    private String entSysDate;
+    private String entDataNo;
+    private String entComment;
+    private String rcvSysCd;
+    private String rcvStaCd;
+    private String rcvSysDate;
+    private String rcvComment;
+    private String traNesCd;
+    private String traStaCd;
+    private String traSysDate;
+    private String dataArea;
+    private List ssDataexchTr = new LinkedList<SsDataexchTr>();
+
+    public String getDataCd() {
+        return dataCd;
+    }
+
+    public void setDataCd(String dataCd) {
+        this.dataCd = dataCd;
+    }
+
+    public String getDataNo() {
+        return dataNo;
+    }
+
+    public void setDataNo(String dataNo) {
+        this.dataNo = dataNo;
+    }
+
+    public String getEntSysCd() {
+        return entSysCd;
+    }
+
+    public void setEntSysCd(String entSysCd) {
+        this.entSysCd = entSysCd;
+    }
+
+    public String getEntSysDate() {
+        return entSysDate;
+    }
+
+    public void setEntSysDate(String entSysDate) {
+        this.entSysDate = entSysDate;
+    }
+
+    public String getEntDataNo() {
+        return entDataNo;
+    }
+
+    public void setEntDataNo(String entDataNo) {
+        this.entDataNo = entDataNo;
+    }
+
+    public String getEntComment() {
+        return entComment;
+    }
+
+    public void setEntComment(String entComment) {
+        this.entComment = entComment;
+    }
+
+    public String getRcvSysCd() {
+        return rcvSysCd;
+    }
+
+    public void setRcvSysCd(String rcvSysCd) {
+        this.rcvSysCd = rcvSysCd;
+    }
+
+    public String getRcvStaCd() {
+        return rcvStaCd;
+    }
+
+    public void setRcvStaCd(String rcvStaCd) {
+        this.rcvStaCd = rcvStaCd;
+    }
+
+    public String getRcvSysDate() {
+        return rcvSysDate;
+    }
+
+    public void setRcvSysDate(String rcvSysDate) {
+        this.rcvSysDate = rcvSysDate;
+    }
+
+    public String getRcvComment() {
+        return rcvComment;
+    }
+
+    public void setRcvComment(String rcvComment) {
+        this.rcvComment = rcvComment;
+    }
+
+    public String getTraNesCd() {
+        return traNesCd;
+    }
+
+    public void setTraNesCd(String traNesCd) {
+        this.traNesCd = traNesCd;
+    }
+
+    public String getTraStaCd() {
+        return traStaCd;
+    }
+
+    public void setTraStaCd(String traStaCd) {
+        this.traStaCd = traStaCd;
+    }
+
+    public String getTraSysDate() {
+        return traSysDate;
+    }
+
+    public void setTraSysDate(String traSysDate) {
+        this.traSysDate = traSysDate;
+    }
+
+    public String getDataArea() {
+        return dataArea;
+    }
+
+    public void setDataArea(String dataArea) {
+        this.dataArea = dataArea;
+    }
+
+    public List getSsDataexchTr() {
+        return ssDataexchTr;
+    }
+
+    public void setSsDataexchTr(List ssDataexchTr) {
+        this.ssDataexchTr = ssDataexchTr;
+    }
+}
---
+++ src/java/com/smi/travel/model/nirvana/SsDataexchTr.java
@@ -0,0 +1,149 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.smi.travel.model.nirvana;
+
+/**
+ *
+ * @author chonnasith
+ */
+public class SsDataexchTr {
+    private String dataCd;
+    private String dataNo;
+    private String dataSeq;
+    private String entSysCd;
+    private String entSysDate;
+    private String entDataNo;
+    private String entComment;
+    private String rcvSysCd;
+    private String rcvStaCd;
+    private String cvSysDate;
+    private String rcvComment;
+    private String traNesCd;
+    private String traStaCd;
+    private String traSysDate;
+    private String dataArea;
+
+    public String getDataCd() {
+        return dataCd;
+    }
+
+    public void setDataCd(String dataCd) {
+        this.dataCd = dataCd;
+    }
+
+    public String getDataNo() {
+        return dataNo;
+    }
+
+    public void setDataNo(String dataNo) {
+        this.dataNo = dataNo;
+    }
+
+    public String getDataSeq() {
+        return dataSeq;
+    }
+
+    public void setDataSeq(String dataSeq) {
+        this.dataSeq = dataSeq;
+    }
+
+    public String getEntSysCd() {
+        return entSysCd;
+    }
+
+    public void setEntSysCd(String entSysCd) {
+        this.entSysCd = entSysCd;
+    }
+
+    public String getEntSysDate() {
+        return entSysDate;
+    }
+
+    public void setEntSysDate(String entSysDate) {
+        this.entSysDate = entSysDate;
+    }
+
+    public String getEntDataNo() {
+        return entDataNo;
+    }
+
+    public void setEntDataNo(String entDataNo) {
+        this.entDataNo = entDataNo;
+    }
+
+    public String getEntComment() {
+        return entComment;
+    }
+
+    public void setEntComment(String entComment) {
+        this.entComment = entComment;
+    }
+
+    public String getRcvSysCd() {
+        return rcvSysCd;
+    }
+
+    public void setRcvSysCd(String rcvSysCd) {
+        this.rcvSysCd = rcvSysCd;
+    }
+
+    public String getRcvStaCd() {
+        return rcvStaCd;
+    }
+
+    public void setRcvStaCd(String rcvStaCd) {
+        this.rcvStaCd = rcvStaCd;
+    }
+
+    public String getCvSysDate() {
+        return cvSysDate;
+    }
+
+    public void setCvSysDate(String cvSysDate) {
+        this.cvSysDate = cvSysDate;
+    }
+
+    public String getRcvComment() {
+        return rcvComment;
+    }
+
+    public void setRcvComment(String rcvComment) {
+        this.rcvComment = rcvComment;
+    }
+
+    public String getTraNesCd() {
+        return traNesCd;
+    }
+
+    public void setTraNesCd(String traNesCd) {
+        this.traNesCd = traNesCd;
+    }
+
+    public String getTraStaCd() {
+        return traStaCd;
+    }
+
+    public void setTraStaCd(String traStaCd) {
+        this.traStaCd = traStaCd;
+    }
+
+    public String getTraSysDate() {
+        return traSysDate;
+    }
+
+    public void setTraSysDate(String traSysDate) {
+        this.traSysDate = traSysDate;
+    }
+
+    public String getDataArea() {
+        return dataArea;
+    }
+
+    public void setDataArea(String dataArea) {
+        this.dataArea = dataArea;
+    }
+}
