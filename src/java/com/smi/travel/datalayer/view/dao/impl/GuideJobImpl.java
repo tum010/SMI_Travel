d022094bb8b58c15814dcc74ad3661a8654c647b
Jittma <jittima.sm@gmail.com>
2016-06-24 15:46:41 +0700

    fix guide job report

--- src/java/com/smi/travel/datalayer/view/dao/impl/GuideJobImpl.java
+++ src/java/com/smi/travel/datalayer/view/dao/impl/GuideJobImpl.java
@@ -67,8 +67,8 @@ public class GuideJobImpl implements GuideJobDao{
                 .addScalar("coupon", Hibernate.STRING)
                 .addScalar("coup_name", Hibernate.STRING)
                 .addScalar("driverid", Hibernate.STRING)
+                .addScalar("tourdescid", Hibernate.STRING)
                 .list();
-        
          for (Object[] B : QueryGuideJobList) {
              GuideJob guidejob = new GuideJob();
              no +=1;
@@ -102,7 +102,8 @@ public class GuideJobImpl implements GuideJobDao{
              guidejob.setPay(util.ConvertString(B[19]));
              guidejob.setMeal(util.ConvertString(B[20]));
              String driverid = util.ConvertString(B[26]);
-             guidejob.setRemark(getremark(tourdate,tourID,driverid));
+             String tourdescid = util.ConvertString(B[27]);
+             guidejob.setRemark(getremark(tourdate,tourID,driverid,tourdescid));
              if(B[22] != null){
                 String coup = (B[25] != null ? "\n"+util.ConvertString(B[25]) : "");
                 if(B[25] != null){
@@ -145,13 +146,27 @@ public class GuideJobImpl implements GuideJobDao{
         return coupNew;
     }
     
-    public String getremark(String tourdate ,String tourid,String driverid){
+    public String getremark(String tourdate ,String tourid,String driverid,String tourdescid){
         Session session = this.sessionFactory.openSession();
         String remark = "";
         UtilityFunction util = new UtilityFunction();
         SimpleDateFormat df = new SimpleDateFormat();
+        boolean driverhaveone = true;
+        String tourdescsize = " SELECT * FROM `tour_operation_driver` where tour_desc_id = '"+tourdescid+"' ";
+        List<Object[]> QueryTourDescList = session.createSQLQuery(tourdescsize)
+                .addScalar("id", Hibernate.STRING)
+                .list();
+        
         df.applyPattern("yyyy-MM-dd"); 
-        String query = "SELECT `tde`.`tour_date` AS `tour_date`, `dt`.`code` AS `tour_id`, group_concat( `st`.`name`, ' Tel:', `st`.`tel`, ' Car No.:', `tdr`.`car_no`, ' Gas:', ifnull(`tdr`.`gas_value`, 0), ':', `tdr`.`gas_fee`, ' Tip:', ifnull(`tdr`.`tip_value`, 0), ':', `tdr`.`tip_fee`, '\\n' SEPARATOR '' ) AS `remark`, ifnull( concat( `gui2`.`name`, ',', 'Tel:', `gui2`.`tel`, '\\n' ), '' ) AS `guide2`, `tde`.`remark` AS `operation_remark` FROM (((( `tour_operation_desc` `tde` LEFT JOIN `tour_operation_driver` `tdr` ON (( `tdr`.`tour_desc_id` = `tde`.`id` ))) LEFT JOIN `staff` `st` ON (( `st`.`id` = `tdr`.`driver_id` ))) LEFT JOIN `staff` `gui2` ON (( `gui2`.`id` = `tde`.`guide_2` ))) LEFT JOIN `daytour` `dt` ON ((`dt`.`id` = `tde`.`tour_id`))) where  tde.tour_date ='"+String.valueOf(df.format(util.convertStringToDate(tourdate)))+"' and dt.code = '"+tourid+"'  and tdr.driver_id <> '"+driverid+"' GROUP BY `tde`.`tour_date`, `tde`.`tour_id` ";
+        String query = "SELECT `tde`.`tour_date` AS `tour_date`, `dt`.`code` AS `tour_id`, group_concat( `st`.`name`, ' Tel:', `st`.`tel`, ' Car No.:', `tdr`.`car_no`, ' Gas:', ifnull(`tdr`.`gas_value`, 0), ':', `tdr`.`gas_fee`, ' Tip:', ifnull(`tdr`.`tip_value`, 0), ':', `tdr`.`tip_fee`, '\\n' SEPARATOR '' ) AS `remark`, ifnull( concat( `gui2`.`name`, ',', 'Tel:', `gui2`.`tel`, '\\n' ), '' ) AS `guide2`, `tde`.`remark` AS `operation_remark` FROM (((( `tour_operation_desc` `tde` LEFT JOIN `tour_operation_driver` `tdr` ON (( `tdr`.`tour_desc_id` = `tde`.`id` ))) LEFT JOIN `staff` `st` ON (( `st`.`id` = `tdr`.`driver_id` ))) LEFT JOIN `staff` `gui2` ON (( `gui2`.`id` = `tde`.`guide_2` ))) LEFT JOIN `daytour` `dt` ON ((`dt`.`id` = `tde`.`tour_id`))) where  tde.tour_date ='"+String.valueOf(df.format(util.convertStringToDate(tourdate)))+"' and dt.code = '"+tourid+"'  ";
+        
+        if(QueryTourDescList != null && QueryTourDescList.size() > 1){
+            driverhaveone = false;
+            query += "  and tdr.driver_id <> '"+driverid+"'  ";
+        }
+        
+        query += "  GROUP BY `tde`.`tour_date`, `tde`.`tour_id` ";
+
 //        String query = "SELECT `tde`.`tour_date` AS `tour_date`, `dt`.`code` AS `tour_id`, group_concat( `st`.`name`, ' Tel:', `st`.`tel`, ' Car No.:', `tdr`.`car_no`, ' Gas:', `tdr`.`gas_value`, ':', `tdr`.`gas_fee`, ' Tip:', `tdr`.`tip_value`, ':', `tdr`.`tip_fee`, '\\n' SEPARATOR '' ) AS `remark`, ifnull( concat( `gui2`.`name`, ',', 'Tel:', `gui2`.`tel`, '\\n' ), '' ) AS `guide2`, `tde`.`remark` AS `operation_remark` FROM (((( `tour_operation_desc` `tde` LEFT JOIN `tour_operation_driver` `tdr` ON (( `tdr`.`tour_desc_id` = `tde`.`id` ))) LEFT JOIN `staff` `st` ON (( `st`.`id` = `tdr`.`driver_id` ))) LEFT JOIN `staff` `gui2` ON (( `gui2`.`id` = `tde`.`guide_2` ))) LEFT JOIN `daytour` `dt` ON ((`dt`.`id` = `tde`.`tour_id`))) where  tde.tour_date ='"+String.valueOf(df.format(util.convertStringToDate(tourdate)))+"' and dt.code = '"+tourid+"'  and tdr.driver_id <> '"+driverid+"' GROUP BY `tde`.`tour_date`, `tde`.`tour_id` ";
         System.out.println("query : "+query);
         List<Object[]> QueryRemarkList = session.createSQLQuery(query)
@@ -166,7 +181,9 @@ public class GuideJobImpl implements GuideJobDao{
             String Guide2Detail = "";
             String OperationRemark = "";
             for (Object[] B : QueryRemarkList) {
-                GuideRemark = util.ConvertString(B[0]);
+                if(!driverhaveone){
+                    GuideRemark = util.ConvertString(B[0]);
+                }
                 Guide2Detail = util.ConvertString(B[1]);  
                 OperationRemark  = util.ConvertString(B[2]);  
             }
@@ -183,6 +200,7 @@ public class GuideJobImpl implements GuideJobDao{
             if((OperationRemark != null)&&(!"".equalsIgnoreCase(OperationRemark))){
                 OperationRemark +=  "\n";
             }
+            
             remark = OperationRemark + Guide2Detail +GuideRemark;
         }
         return remark;
